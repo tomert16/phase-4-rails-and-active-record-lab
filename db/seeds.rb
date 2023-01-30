1d1ab4eb22915efc8e5706fd1bedcddc48c8daa5
tomer <tomertal52@yahoo.com>
2023-01-30 12:02:13 -0500

    commit tree 104081fe9f911798029eab1d355ceef47c35ea4f

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.create(first_name: 'Luke', last_name: "Skywalker", grade: 11)
